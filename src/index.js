931362aeb77a37ad755564e68b9d966c85979f8d
Nick Maskell <nrmaskell@gmail.com>
2018-02-21 20:30:58 +1300

    Add user authentication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParse from "body-parser";
+import dotenv from "dotenv";
+import bluebird from "bluebird";
 
+import auth from "../routes/auth";
+
+dotenv.config();
 const app = express();
+
+app.use(bodyParse.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+mongoose.Promise = bluebird;
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use("/api/auth", auth);
 
 app.post("/api/auth", (req, res) => {
   res.status(400).json({ errors: { global: "Invalid credentials" } });
